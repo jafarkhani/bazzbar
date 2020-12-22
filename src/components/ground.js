ccfde84808e96522ba41b47d211c53b01e30197c
jafarkhani <jafarkhani.shabnam@gmail.com>
2020-12-22 17:23:21 +0330

    first commit

---
+++ src/components/ground.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Ground = ()=> {
+
+    return (
+    <table>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+        <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>);
+}
+
+export default Ground;
