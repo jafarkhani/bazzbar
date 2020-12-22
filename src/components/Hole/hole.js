c0f4d9956f796e5faec96c493e2a434068e04b77
jafarkhani <jafarkhani.shabnam@gmail.com>
2020-12-22 19:01:36 +0330

    --

--- src/components/Hole/hole.js
+++ src/components/Hole/hole.js
@@ -1,13 +1,12 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './hole.scss';
 
 
 const Hole = (props) => {
 
-
     return (
-    <div className="Hole" key={props.id}>
+    <div className="Hole" key={props.id}>        
         {
             props.molPos !== props.id ? null : 
                 <div className="mole" onClick={props.AddScore}></div>
--- src/components/ground.js
+++ src/components/ground.js
@@ -1,50 +1,81 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Hole from './Hole/hole';
 
 const Ground = (props) => {
 
     const HoleCount = 7;
+    const timeout = useRef(null);
 
     const [timer, SetTimer] = useState(60);
     const [score, SetScore] = useState(0);
+    const [totalMoles, SettotalMoles] = useState(0);
     const [molePos, SetmolePos] = useState(0);
+
 
     useEffect(()=>{
         setInterval(()=>{
-            SetTimer(timer-1);
-        }, 1000);
-    }, []);
+            SetTimer(timer-1);                       
+        }, 60000);
+
+        SettotalMoles(totalMoles+1); 
+
+    }, [props.player]);
 
     useEffect(()=>{
         if(timer > 0)
             return;
-        console.log("end");
+        
+        //-------
+
     },[timer]);
 
-    useEffect(()=> {
-
+    
+    useEffect(() => {
         let random = Math.floor(Math.random() * 7) + 1 ; 
+        console.log(random);
         SetmolePos(random);
 
-    }, [score]);
+        timeout.current = setTimeout(()=>{
+            console.log("timeout")
+            FailScore();
+        },2000);
+
+    },[totalMoles]);
 
     const AddScore = () => {
-        SetScore(score+1);
+        SetScore(score+1);           
+        clearTimeout(timeout.current );     
+        SettotalMoles(totalMoles+1);
     }
 
+    const FailScore = () => {
+        
+        SettotalMoles(totalMoles+1);        
+    }
+
+
     let holes = [];
-    for (let i = 1; i < HoleCount; i++) {
+    for (let i = 1; i <= HoleCount; i++) {
         holes.push(<Hole 
+            key={i}
             id={i} 
+            totalMoles={totalMoles}
+            score={score}
             molPos={molePos}
             AddScore={AddScore}
+            FailScore={FailScore}
             />);
     }
 
+    if(timer === 0)
+        return <div>End Game</div>
+
     return (
         <div align="center">
-            <h3>Play Time remain: {timer} </h3>
-            <br />
+            <h3>Player: {props.player} </h3>      
+            <h3>Play Time remain: {timer} </h3>            
+            <h3>Total Moles: {totalMoles} </h3>            
+            <h3>Your Score: {score} </h3>
             {holes}
         </div>);
 }
